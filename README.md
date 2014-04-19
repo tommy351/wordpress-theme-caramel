216acc14e51b4f8b7a3ce8e95cceb23397646875
Tommy Chen <tommy351@gmail.com>
2014-04-19 13:47:11 +0800

    Add gulp file

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-wordpress-theme-caramel
-=======================
+# Caramel
---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+  stylus = require('gulp-stylus'),
+  nib = require('nib');
+
+gulp.task('stylus', function(){
+  gulp.src('./styl/style.styl')
+    .pipe(stylus({
+      use: [nib()]
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('dev', function(){
+  gulp.watch('./styl/**/*.styl', ['stylus']);
+});
+
+gulp.task('default', ['stylus']);
